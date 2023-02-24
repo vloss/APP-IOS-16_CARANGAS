b64ffeaa7e0e23805ac047580ce2162446e741c5
vloss <viniciusvianaloss@gmail.com>
2023-02-24 02:16:52 -0300

    Inclusão de projeto do curso.

---
+++ Carangas/Carangas/AddEditViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddEditViewController.swift
+//  Carangas
+//
+//  Created by Eric Brito.
+//  Copyright © 2017 Eric Brito. All rights reserved.
+//
+
+import UIKit
+
+class AddEditViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var tfBrand: UITextField!
+    @IBOutlet weak var tfName: UITextField!
+    @IBOutlet weak var tfPrice: UITextField!
+    @IBOutlet weak var scGasType: UISegmentedControl!
+    @IBOutlet weak var btAddEdit: UIButton!
+    @IBOutlet weak var loading: UIActivityIndicatorView!
+
+    // MARK: - Super Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - IBActions
+    @IBAction func addEdit(_ sender: UIButton) {
+    }
+
+}
---
+++ Carangas/Carangas/CarViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CarViewController.swift
+//  Carangas
+//
+//  Created by Eric Brito on 21/10/17.
+//  Copyright © 2017 Eric Brito. All rights reserved.
+//
+
+import UIKit
+
+class CarViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var lbBrand: UILabel!
+    @IBOutlet weak var lbGasType: UILabel!
+    @IBOutlet weak var lbPrice: UILabel!
+
+    // MARK: - Super Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
