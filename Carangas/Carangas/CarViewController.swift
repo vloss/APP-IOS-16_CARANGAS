67f6e9f3c822839f1ebb89ab2a7437d3e7c70f61
vloss <viniciusvianaloss@gmail.com>
2023-02-27 23:28:13 -0300

    Finalizando projeto com inclusão de webview.

--- Carangas/Carangas/CarViewController.swift
+++ Carangas/Carangas/CarViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Foundation
+import WebKit
 
 class CarViewController: UIViewController {
 
@@ -14,7 +16,8 @@ class CarViewController: UIViewController {
     @IBOutlet weak var lbBrand: UILabel!
     @IBOutlet weak var lbGasType: UILabel!
     @IBOutlet weak var lbPrice: UILabel!
-    
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var loading: UIActivityIndicatorView!
     
     //MARK: - Properties
     var car: Car!
@@ -31,6 +34,17 @@ class CarViewController: UIViewController {
         lbBrand.text = car.brand
         lbGasType.text = car.gas
         lbPrice.text = "R$ \(car.price)"
+        
+        let name = (title! + "+" + car.brand).replacingOccurrences(of: " ", with: "+")
+        let urlString = "https://www.google.com/search?q=\(name)&tbm=isch"
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        
+        webView.allowsBackForwardNavigationGestures = true
+        webView.allowsLinkPreview = true
+        webView.navigationDelegate = self
+        webView.uiDelegate = self
+        webView.load(request)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -38,4 +52,10 @@ class CarViewController: UIViewController {
         vc.car = car
     }
 
+}
+
+extension CarViewController: WKNavigationDelegate, WKUIDelegate{
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        loading.stopAnimating()
+    }
 }
